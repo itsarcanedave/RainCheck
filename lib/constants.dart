240f91be9fad75dab196eee43c538f00a6a9b127
Richard David <48126051+itsarcanedave@users.noreply.github.com>
2021-07-27 10:13:56 +0700

    Scraped API keys

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const ApiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const ApiKey = 'c818eaa7c4529f15719b45c505b15095';
+const ApiKey = '[REDACTED]';
 
 
 
